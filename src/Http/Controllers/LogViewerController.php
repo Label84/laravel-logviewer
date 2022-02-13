2128924fc7d018b8d3fded0ec07412eed4ec1eb3
Tjardo <dev@tjardo.com>
2022-02-13 20:09:27 +0100

    Fix controller when clauses

--- src/Http/Controllers/LogViewerController.php
+++ src/Http/Controllers/LogViewerController.php
@@ -12,12 +12,12 @@ class LogViewerController extends Controller
     public function index(Request $request): View
     {
         $items = LogViewer::logs()
-            ->when($request->query('level'), fn ($collection) => $collection->whereLevel($request->query('level')))
-            ->when($request->query('date'), fn ($collection) => $collection->whereDate($request->query('date')))
-            ->when($request->query('from'), fn ($collection) => $collection->whereDateFrom($request->query('from')))
-            ->when($request->query('till'), fn ($collection) => $collection->whereDateTill($request->query('till')))
-            ->when($request->query('logger'), fn ($collection) => $collection->whereLogger($request->query('logger')))
-            ->when($request->query('message'), fn ($collection) => $collection->whereMessage($request->query('message')))
+            ->when($request->has('level'), fn ($collection) => $collection->whereLevel($request->query('level')))
+            ->when($request->has('date'), fn ($collection) => $collection->whereDate($request->query('date')))
+            ->when($request->has('from'), fn ($collection) => $collection->whereDateFrom($request->query('from')))
+            ->when($request->has('till'), fn ($collection) => $collection->whereDateTill($request->query('till')))
+            ->when($request->has('logger'), fn ($collection) => $collection->whereLogger($request->query('logger')))
+            ->when($request->has('message'), fn ($collection) => $collection->whereMessage($request->query('message')))
             ->paginate(config('logviewer.view.items_per_page'));
 
         return view('logviewer::index', compact('items'));
